76ddeafbc13e3725f48581065f1b48d72523817d
Waqas Mumtaz <42939215+waqas-mumtaz@users.noreply.github.com>
2025-09-15 21:18:38 +0200

    update nav links

---
+++ src/assets/data/index.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "About Us",
+    path: "/about-us",
+  },
+  {
+    name: "Our Work",
+    path: "/our-work",
+  },
+  {
+    name: "Our Team",
+    path: "/our-team",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact-us",
+  },
+];
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import logo from "@/assets/imgs/CXD-Logo-Blue-black.png";
+import { navLinks } from "@/assets/data";
 
 const Header = () => {
   return (
@@ -8,39 +9,15 @@ const Header = () => {
 
       <div className="flex items-center gap-6 ml-auto">
         <div className="hidden lg:flex gap-11 lato-bold items-center">
-          <Link className="text-blue-700" to="/">
-            Home
-          </Link>
-          <Link
-            className="hover:text-blue-700 transition-colors duration-500"
-            to="#"
-          >
-            About Us
-          </Link>
-          <Link
-            className="hover:text-blue-700 transition-colors duration-500"
-            to="/services"
-          >
-            Services
-          </Link>
-          <Link
-            className="hover:text-blue-700 transition-colors duration-500"
-            to="#"
-          >
-            Our Work
-          </Link>
-          <Link
-            className="hover:text-blue-700 transition-colors duration-500"
-            to="#"
-          >
-            Our Team
-          </Link>
-          <Link
-            className="hover:text-blue-700 transition-colors duration-500"
-            to="#"
-          >
-            Contact Us
-          </Link>
+          {navLinks.map((link) => (
+            <Link
+              key={link.name}
+              className="hover:text-blue-700 transition-colors duration-500"
+              to={link.path}
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
 
         <div className="py-2 px-3 bg-black text-white rounded-md cursor-pointer hover:bg-blue-700 hover:scale-110 active:scale-95 transition-all duration-300 ml-5">
